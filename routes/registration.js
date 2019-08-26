abb8b66f0075f6a6d9ad9f1081926479d411b82c
szewah <swgrleong@gmail.com>
2019-08-26 15:30:50 -0400

    registration route working

--- routes/registration.js
+++ routes/registration.js
@@ -8,19 +8,19 @@ router.get('/registration', function(req, res, next) {
   res.sendFile(path.join(__dirname, "../views/register.html"))
 });
 
-router.post('/api/registration', async(req, res, err) => {
-  if (err) throw err;
+router.post('/api/registration', function(req, res, err) {
+  if (err) {console.log("error occurred")};
   console.log(req.body);
-  db.Users.create({
-    firstName: req.body.registerName,
+  db.User.create({
+    firstname: req.body.registerName,
     lastName: req.body.registerSurname,
     email: req.body.registerEmail,
-    password: req.body.egisterPassword
+    password: req.body.registerPassword
   }).then((results) => {
-      res.json(results);
       res.redirect('/');
   }).catch((err) => {
     res.json(err);
+    console.log("Another error occured");
   });
 })
 
